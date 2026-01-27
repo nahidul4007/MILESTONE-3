5a420420a9e882f8cbf687de0839b02bc3cc5118
nahidul4007 <mdnahidulislam4007@gmail.com>
2026-01-27 23:18:04 +0600

    normal js:

--- Script/logic.js
+++ Script/logic.js
@@ -4,10 +4,39 @@ console.log(num);
 string = 'hello java script coder'
 console.log(string);
 float=12.5;
-console.log(float);
+console.log(typeof float);
 var nothing = 200;
 console.log(nothing);
 sum= num+float+nothing;
 console.log("Your Sum is : "+sum);
 var age;
-console.log(age)
+console.log(age);
+var template = "#-----------------------------#--------------------------#";
+var template1 = "NAME : MD.NAHIDUL ISLAM";
+var template2 = "PHONE: +8801328276240";
+var template3 = "USER : NAHIUDUL";
+var template4 = "STUDENT OF RAJSHAI POLYTECHNIC INSTITUTE";
+console.log(template , template1 , template2 , template3 , template4);
+
+// next module  
+
+// string to integer
+var number = parseInt("76");
+var NUM = parseInt("asdfjsIII");
+console.log(typeof number)
+console.log(NUM)
+
+var a=0.1;
+var b=0.2;
+var total = a+b
+console.log(total.toFixed(4));
+
+
+
+
+
+
+
+
+
+
