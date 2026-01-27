94b5225746efd85d48c1aca6a7f389c4ccbb0598
nahidul4007 <mdnahidulislam4007@gmail.com>
2026-01-27 22:07:42 +0600

    module 3 ar class 3 tah ses korlam .

---
+++ Script/logic.js
@@ -0,0 +1,13 @@
+console.log('hello');
+num=70;
+console.log(num);
+string = 'hello java script coder'
+console.log(string);
+float=12.5;
+console.log(float);
+var nothing = 200;
+console.log(nothing);
+sum= num+float+nothing;
+console.log("Your Sum is : "+sum);
+var age;
+console.log(age)
