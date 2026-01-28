daa5aed1c1f9a0d4346a86f9813de31d998df9c5
nahidul4007 <mdnahidulislam4007@gmail.com>
2026-01-28 14:03:50 +0600

    module 13 ses korlam

---
+++ Script/task.js
@@ -0,0 +1,49 @@
+Design = "#------------------[Task-1]-------------------#"
+Account = 1000;
+apple=50;
+angur=30;
+kola=20;
+alu=60;
+chanachur=30;
+bazar=apple+angur+kola+alu+chanachur;
+ache=Account-bazar
+console.log(Design)
+console.log("Tomar Bank a taka ache "+Account+" Taka")
+console.log("Tumi bazar korso "+bazar+" Takar" )
+console.log('Tomar Bank a taka ache '+ache+" Taka")
+console.log(Design)
+Design = "#------------------[Task-2]-------------------#"
+Bangla=70;
+English=80;
+Math=33.5;
+Social=80;
+Science=99;
+Islam=100;
+Mark=Bangla+English+Math+Social+Science+Islam;
+Rank=Mark/6;
+console.log(Design);
+console.log("Tumi banglai paiso= "+Bangla)
+console.log("Tumi English paiso= "+English)
+console.log("Tumi Math paiso= "+Math)
+console.log("Tumi Social paiso= "+Social)
+console.log("Tumi Science paiso= "+Science)
+console.log("Tumi Islam paiso= "+Islam)
+console.log("#----------------------------------#")
+console.log("Tumi Full Mark Paiso= " +Mark)
+console.log("Tomar Class a rank Number= " +Rank.toFixed(2) +" %")
+console.log(Design)
+
+
+Design = "#------------------[Task-3]-------------------#"
+num=79;
+division=num%5;
+console.log(Design)
+console.log(division)
+console.log(Design)
+
+
+Design = "#------------------[Task-4]-------------------#"
+const a=isNaN("11");
+console.log(a)
+const A=isNaN(2-10);
+console.log(A)
