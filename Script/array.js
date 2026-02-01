1b9180559c3bdd027170976cdeae0e58316083ee
nahidul4007 <mdnahidulislam4007@gmail.com>
2026-02-01 21:50:41 +0600

    14.5.2 SES KORE Akn 14.5.3 start korlam

---
+++ Script/array.js
@@ -0,0 +1,8 @@
+var foodList=['NAHID', 'ROJA', 'TAHA'];
+var numbers=[12, 33, 66, 44];
+var constant=[true, false, false, true]
+// var foodLis
+foodList[3]='ROJA'
+console.log(foodList[1])
+console.log(foodList.length)
+console.log(foodList)
