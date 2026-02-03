98b892403a6b6e28998934579b7f76da228b2167
nahidul4007 <mdnahidulislam4007@gmail.com>
2026-02-03 10:22:03 +0600

    MODULE 15 AR 15_3 Problem solving with While Loop SES KORLAM

---
+++ Script/class-15.js
@@ -0,0 +1,77 @@
+//  const : value can change
+//  let : can change .
+// var : shuld not use it 
+
+
+// variable
+const ammount=200;
+
+
+if(ammount>100){
+
+}
+else{
+
+}
+
+// array
+const market=[12, 34, 56, 11]
+
+//  previous known in module 14
+
+//  module 15 
+
+
+const numbe =[12, 56, 23, 22]
+
+for(const i of numbe){
+    console.log(i)
+}
+
+
+const fruit = ["apple", 'orrange', 'kola']
+
+for(const market of fruit){
+    console.log(market, "i want to eat")
+}
+var number=[10, 11, 34, 33, 66, 79, 44, 22, 99];
+for(const mark of number){
+    if(mark<70){
+        console.log(mark);
+    }
+    else{
+        console.log("akta mark ar bahire gese", mark)
+    }
+}
+
+//  infinity loop 
+// let num=1;
+
+// while(num){
+//     console.log( num,'FILE DONE');
+//     // num=num+1
+//     num++;
+// }
+
+// even number
+let numb=1;
+while(numb <=10){
+    console.log(numb)
+    if(numb%2===0){
+        console.log("even number is" , numb)
+    }
+    numb++
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
