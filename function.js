6d41a041d6fed9b33303b52c8f6bbb854f198411
CYBERCOP-404 <cybercopbangladesh@gmail.com>
2026-02-09 18:43:19 +0600

    function start holo

---
+++ function.js
@@ -0,0 +1,17 @@
+const number=5;
+
+function fanOffkor(){
+    console.log("bosa the uthe dara ");
+    console.log("click the switch ");
+}
+
+// function ai vab a dite hoi . 
+//  output paite hole function coll korte hobe . 
+function brushTeeth(){
+    console.log("pic up the brush");
+    console.log("brush");
+}
+brushTeeth();
+fanOffkor();
+brushTeeth();
+
